7cb75802e3a1ec130eadf62d2a86db66f9b4f43b
VitorHRD <vrdev435@gmail.com>
2022-04-25 05:56:13 -0300

    origin

---
+++ app.js
@@ -0,0 +1,46 @@
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const puppeteer = require('puppeteer');
+const PORT = process.env.PORT;
+
+function LoadMore(page , selector){
+   const moreButton = page.$(selector);
+   if(moreButton){
+       moreButton.click()
+   }
+}
+
+
+async function start(){
+
+   async function LoadMore(page , selector){
+        const moreButton = await page.$(selector);
+        console.log(moreButton)
+        if(moreButton){
+            console.log("more")
+           await moreButton.click();
+           await page.waitFor(selector,{timeout:3000})
+           await LoadMore(page,selector)
+        }
+     }
+
+
+    async function getComments(page,selector){
+        const comments = await page.$$eval(selector, links => links.map( link=> link.innerText))
+        return comments
+    }
+ 
+
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+     await page.goto('https://www.instagram.com/p/CcqpSRKu46f/')
+     await LoadMore(page ,'.wpO6b')
+    const comments = await getComments(page,'.MOdxS span a' )
+    console.log(comments)
+    
+}
+
+
+ start()
